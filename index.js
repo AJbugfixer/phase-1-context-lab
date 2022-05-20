8a071c327717086a797dbd0f5b843b58f57395b4
Ajay <ajay.ip3105@gmail.com>
2022-05-19 20:31:59 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,59 @@
 /* Your Code Here */
+function createEmployeeRecord(recordArr) {
+    return {
+        firstName: recordArr[0],
+        familyName: recordArr[1],
+        title: recordArr[2],
+        payPerHour: recordArr[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+
+function createEmployeeRecords(arrOfRecordArr) {
+    return arrOfRecordArr.map(recordArr => createEmployeeRecord(recordArr));
+}
+
+function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+
+    const timeInObject = {
+        type: 'TimeIn',
+        hour: +hour,
+        date: date,
+    }
+
+    this.timeInEvents.push(timeInObject);
+    return this;
+}
+
+
+function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+
+    const timeOutObject = {
+        type: 'TimeOut',
+        hour: +hour,
+        date: date,
+    }
+
+    this.timeOutEvents.push(timeOutObject);
+    return this;
+}
+
+
+function hoursWorkedOnDate(dateNoHour) {
+    const timeInHour = this.timeInEvents.find(dateStamp => dateStamp.date === dateNoHour).hour;
+    const timeOutHour = this.timeOutEvents.find(dateStamp => dateStamp.date === dateNoHour).hour;
+
+    return ((timeOutHour - timeInHour) / 100);
+}
+
+function wagesEarnedOnDate(dateNoHour){
+    const wagesOnDate = hoursWorkedOnDate.call(this, dateNoHour)
+    return wagesOnDate * this.payPerHour
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +76,10 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(arrOfRecordArr, firstName){
+    return arrOfRecordArr.find(arr => arr.firstName === firstName)
+}
+
+function calculatePayroll(arrOfRecordArr) {
+    return arrOfRecordArr.reduce((ticket, recordArr) => ticket + allWagesFor.call(recordArr), 0)
+}
